eef3d4ade5460f0be01fa758504d207ea79ee01b
Diogo Ide <diogoide@gmail.com>
2018-03-22 00:38:13 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -78,8 +78,9 @@ class TicTacToe
     position_2 = @board[win_index_2]
     position_3 = @board[win_index_3]
 
-    if @board[win_combination[0]] == @board[win_combination[1]] && @board[win_combination[0]] == @board[win_combination[2]]
+    #if @board[win_combination[0]] == @board[win_combination[1]] && @board[win_combination[0]] == @board[win_combination[2]]
       #binding.pry
+      if position_1 == "X" && position_2 == "X" && position_3 == "X" || (position_1 == "O" && position_2 == "O" && position_3 == "O"
       return win_combination
     end
   end
