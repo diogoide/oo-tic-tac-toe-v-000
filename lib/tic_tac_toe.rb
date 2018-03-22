db8c78ffef1a54c9003a72b9dd71ab2bf12dfe2b
Diogo Ide <diogoide@gmail.com>
2018-03-22 00:39:54 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -83,6 +83,7 @@ class TicTacToe
       if position_1 == "X" && position_2 == "X" && position_3 == "X" || position_1 == "O" && position_2 == "O" && position_3 == "O"
       return win_combination
     end
+    false 
   end
 
   def full?
