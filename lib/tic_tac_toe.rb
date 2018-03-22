dabe457f0c0a2ea2ce06ab5d9557162628cff4f3
Diogo Ide <diogoide@gmail.com>
2018-03-22 01:00:32 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -117,9 +117,8 @@ class TicTacToe
       return "X"
     elsif won? && (position_1 == "O" && position_2 == "O" && position_3 == "O")
       return "O"
-    else 
-      nil  
     end
+    nil
   end
     end
   end
