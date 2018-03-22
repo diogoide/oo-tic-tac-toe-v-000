0110e50ebdc1d0cfa8d5813f363532a9a108376e
Diogo Ide <diogoide@gmail.com>
2018-03-22 00:26:03 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -102,6 +102,26 @@ class TicTacToe
   end
 
 
+#   def winner?
+#     WIN_COMBINATIONS.each do |win_combination|
+#
+#     win_index_1 = win_combination[0]
+#     win_index_2 = win_combination[1]
+#     win_index_3 = win_combination[2]
+#
+#     position_1 = @board[win_index_1]
+#     position_2 = @board[win_index_2]
+#     position_3 = @board[win_index_3]
+#
+#     if won? && @board[win_combination[0]]
+#       return "X"
+#     else won? && position_1 == "O" && position_2 == "O" && position_3 == "O"
+#       return "O"
+#   end
+#   nil
+#   end
+# end
+
    def winner
      if the_winner = won? #[0,1,2]
        @board[the_winner[0]]
