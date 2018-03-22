368599fd48e4e33600a6544634856fbb0501dd7a
Diogo Ide <diogoide@gmail.com>
2018-03-22 00:11:05 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -26,7 +26,6 @@ class TicTacToe
 
   def input_to_index(input)
   index = input.to_i - 1
-
   end
 
   def move(index,token)
@@ -84,6 +83,7 @@ class TicTacToe
       return win_combination
     end
   end
+end
 
   def full?
       @board.all? do |letter|
